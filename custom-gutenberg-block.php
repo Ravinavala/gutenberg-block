c315447898f05a9c65a870dabecf3bd400a986f6
Ravina Vala <vala8460@gmail.com>
2023-01-22 05:00:39 +0530

    updated gitignore file

--- custom-gutenberg-block.php
+++ custom-gutenberg-block.php
@@ -16,6 +16,12 @@ function custom_block_register_block() {
             array('wp-blocks', 'wp-element', 'wp-editor'),
             filemtime(plugin_dir_path(__FILE__) . 'build/index.js')
     );
+    
+    register_block_type('gutenberg-block/teams-block', array(
+        'editor_script' => 'my-custom-block',
+        'editor_style' => 'my-custom-block-editor-style',
+        'style' => 'my-custom-block-frontend-style',
+    ));
 }
 
 add_action('init', 'custom_block_register_block');
