a5a65c163f3f3320341853633547de47b3f26646
Ravina Vala <vala8460@gmail.com>
2023-01-22 07:00:16 +0530

    managed about us section content

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-element'), 'version' => 'bd8c7cafb9342da50c10');
--- src/index.js
+++ src/index.js
@@ -20,7 +20,56 @@ registerBlockType('gutenberg-block/teams-block', {
         },
     },
     edit: (props) => {
+        const {attributes, setAttributes} = props;
+        const {image, member_name, description, memberPosition} = attributes;
+
+        return (
+                <div className="my-custom-block">
+                    <MediaUpload
+                        onSelect={(media) => setAttributes({image: media})}
+                        type="image"
+                        value={image}
+                        render={({ open }) => (
+                        <button onClick={open}>
+                        {!image ? 'Upload Image' : <img src={image.url} alt={image.alt} />}
+                        </button>
+                        )}
+                    />
+                    <RichText
+                        tagName="p"
+                        placeholder="Member Name..."
+                        value={member_name}
+                        onChange={(newMemberName) => setAttributes({member_name: newMemberName})}
+                        />
+                    <RichText
+                        tagName="p"
+                        placeholder="Member Details..."
+                        value={description}
+                        onChange={(newDescription) => setAttributes({description: newDescription})}
+                        />
+                    <RichText
+                        tagName="p"
+                        placeholder="Member Position..."
+                        value={memberPosition}
+                        onChange={(newMemberPosition) => setAttributes({memberPosition: newMemberPosition})}
+                        />
+                </div>
+                );
     },
     save: (props) => {
+        const {attributes} = props;
+        const {image, member_name, description, memberPosition} = attributes;
+        return (
+            <div className="team-custom-block">
+                <div className="team_image">
+                    <img src={image.url} alt={image.alt} />
+                </div>
+                <div className="team_content">
+                    <RichText.Content tagName="h2" value={member_name} />
+                    <RichText.Content tagName="p" value={description} />
+                    <RichText.Content tagName="h5" value={memberPosition} />
+                </div>
+            </div>
+        );
     },
 });
